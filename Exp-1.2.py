3abfb991c9924b19a39508169475e0257e262975
Teja047 <nagateja047@gmail.com>
2025-04-29 14:58:38 +0530

    Add files via upload

---
+++ Exp-1.2.py
@@ -0,0 +1,16 @@
+a=eval(input("Enter first number:"))
+b=eval(input("Enter second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+fd=a//b
+ex=a**b
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(fd)
+print(ex)
