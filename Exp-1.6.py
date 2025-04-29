ad4d73a69db3b0013ee775ca4cff0a56ea5797a6
Teja047 <nagateja047@gmail.com>
2025-04-29 15:00:40 +0530

    Add files via upload

---
+++ Exp-1.6.py
@@ -0,0 +1,6 @@
+length = eval(input("length: "))
+width = eval(input("width: "))
+area = length*width
+perimeter = 2*(length + width)
+print("Area of the rectangle: ",area)
+print("Perimeter of the rectangle: ",perimeter)
